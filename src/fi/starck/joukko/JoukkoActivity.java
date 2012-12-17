bd35ba73ad26be9fdaef43cd144fca96a5395fcd
Tuomas Starck <dev@starck.fi>
2012-12-17 02:37:12 +0200

    Ragequit ready

--- src/fi/starck/joukko/JoukkoActivity.java
+++ src/fi/starck/joukko/JoukkoActivity.java
@@ -9,81 +9,99 @@ import android.hardware.SensorManager;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.util.FloatMath;
-import android.util.Log;
 
 /**
  * Chess game board in OpenGL ES.
  *
  * @author Tuomas Starck
  */
-public class JoukkoActivity extends Activity {
-    private final String TAG = "JACT";
-    private final float OVER9000 = 12.0f;
+public class JoukkoActivity extends Activity implements SensorEventListener {
+    private final float OVER_9000 = 12.0f;
 
     private GLSurfaceView view;
     private SensorManager sensor;
-    private float mAccel;         // acceleration apart from gravity
-    private float mAccelCurrent;  // current acceleration including gravity
-    private float mAccelLast;     // last acceleration including gravity
+    private float acceleration, accelNow, accelLast;
 
-    private final SensorEventListener sensorlistener = new SensorEventListener() {
-        @Override
-        public void onSensorChanged(SensorEvent se) {
-            float x = se.values[0];
-            float y = se.values[1];
-            float z = se.values[2];
-            mAccelLast = mAccelCurrent;
-            mAccelCurrent = FloatMath.sqrt(x*x + y*y + z*z);
-            float delta = mAccelCurrent - mAccelLast;
-            mAccel = mAccel * 0.9f + delta; // perform low-cut filter
-            if (mAccel > OVER9000) {
-                Log.i(TAG, "Ragequit with force of " + mAccel + " bhp");
-                ((GLView) view).reset();
-            }
-        }
+    private float sqrsum(float x, float y, float z) {
+        return FloatMath.sqrt(x*x + y*y + z*z);
+    }
 
-        @Override
-        public void onAccuracyChanged(Sensor sensor, int accuracy) {}
-    };
+    private void startSensor() {
+        sensor.registerListener(
+            this,
+            sensor.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+            SensorManager.SENSOR_DELAY_NORMAL
+        );
+    }
 
+    /**
+     * On activity creation a new View is created, sensor values are
+     * initiated and accelerometer is activated.
+     *
+     * To save battery, rendering is done only when specifically requested.
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         view = new GLView(this);
-        /* Save battery by rendering only when specifically requested.
-         */
         view.setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
         setContentView(view);
 
+        acceleration = 0.0f;
+        accelNow = SensorManager.GRAVITY_EARTH;
+        accelLast = SensorManager.GRAVITY_EARTH;
+
         sensor = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        sensor.registerListener(sensorlistener, sensor.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
-        mAccel = 0.00f;
-        mAccelCurrent = SensorManager.GRAVITY_EARTH;
-        mAccelLast = SensorManager.GRAVITY_EARTH;
+        startSensor();
     }
 
+    /**
+     * Back button goes back in history.
+     *
+     * @todo Should this do finish() if there is nothing
+     *       else to do? Many apps seems to do that.
+     */
     @Override
     public void onBackPressed() {
         ((GLView) view).undo();
     }
 
     @Override
-    protected void onPause() {
-        super.onPause();
-        view.onPause();
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    /**
+     * Calculate acceleration and trigger reset.
+     */
+    @Override
+    public void onSensorChanged(SensorEvent se) {
+        accelLast = accelNow;
+        accelNow = sqrsum(se.values[0], se.values[1], se.values[2]);
+        acceleration = acceleration * 0.9f + (accelNow - accelLast);
+
+        if (acceleration > OVER_9000) {
+            ((GLView) view).reset();
+        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         view.onResume();
-        sensor.registerListener(sensorlistener, sensor.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+        startSensor();
+    }
+
+    @Override
+    protected void onPause() {
+        sensor.unregisterListener(this);
+        view.onPause();
+        super.onPause();
     }
 
     @Override
     protected void onStop() {
-        sensor.unregisterListener(sensorlistener);
+        sensor.unregisterListener(this);
         super.onStop();
     }
 }
